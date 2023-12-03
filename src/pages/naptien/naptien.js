29cd3f1b2954444cef17f53c5118c6f666a52e8b
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-12-03 22:07:46 +0700

    add mobile response for naptien

--- src/pages/naptien/naptien.js
+++ src/pages/naptien/naptien.js
@@ -8,8 +8,10 @@ const cx = classNames.bind(styles);
 
 function NapTien() {
     const [userName, setUserName] = useState("");
+    const [textForSelectBank, setTextForSelectBank] = useState("Chọn Ngân Hàng Nhận");
     const [bankBin, setBankBin] = useState("");
     const [amount, setAmount] = useState(0);
+    const [moneyInputAmount, setMoneyInputAmount] = useState('');
     const [bankList, setBankList] = useState();
     const [adminName, setAdminName] = useState("Tòng Châu Bình");
     const [showBankInforName, setShowBankInforName] = useState("VietinBank");
@@ -17,6 +19,7 @@ function NapTien() {
     const [submitted, setSubmitted] = useState(false);
     const [qrGenerated, setQrGenerated] = useState(false);
     const [showBankDropDownMenu, setShowBankDropDownMenu] = useState(false);
+    const [showInfor, setShowInfor] = useState(false);
     const [qrURL, setQrURL] = useState(null);
     const vietQR = new VietQR({
         clientID: '4244e11f-e282-4e3c-af39-6d9749c99e44',
@@ -24,15 +27,20 @@ function NapTien() {
     });
     //admin bank info
     const adminBankName = ["VietinBank", "TPBank", "BIDV"];
-    const adminBankAccountName = ["Tòng Châu Bình", "Châu Bình", "Bình"];
+
+    const adminBankAccountName = ["Tong Chau Binh", "Chau Binh", "Binh Chau"];
+    //Tên tài khoản là tiếng Việt không dấu, viết hoa, tối thiểu 5 ký tự, tối đa 50 kí tự, không chứa các ký tự đặc biệt
+
     const adminBankAccount = ["1234567890", "20869042001", "0987654321"]
+    //end admin bank info
+
     //get bank bin
     useEffect(() => {
         if (showBankInforName && adminBankNumber) {
             vietQR
                 .getBanks()
                 .then((banks) => {
-                    setBankList(banks)
+                    setBankList(banks.data)
                     let filtedBank = banks.data.filter(bank => bank.shortName === showBankInforName);
                     if (filtedBank.length > 0) {
                         setBankBin(filtedBank[0].bin)
@@ -58,9 +66,13 @@ function NapTien() {
                     "amount": amount,
                     "addInfo": userName,
                     "format": "text",
-                    "template": "compact"
+                    "template": "compact2"
+                    //compact2 có thêm thông tin người nhận và số tiền trên ảnh
+                    //compact thì không có 
+                    //qr_only thì chỉ có mã, không có logo vietqr, napas cũng như ngan hàng
                 })
                 .then((data) => {
+                    console.log(data);
                     if (data.data.data) {
                         setQrURL(data.data.data.qrDataURL);
                         setQrGenerated(true);
@@ -85,8 +97,28 @@ function NapTien() {
         setUserName(e.target.value)
     };
     const changeAmountHandler = (e) => {
-        setAmount(e.target.value)
-    };
+        let inputValue = e.target.value;
+
+        // Loại bỏ tất cả các ký tự không phải là số
+        inputValue = inputValue.replace(/[^0-9]/g, '');
+
+        // Chuyển đổi giá trị thành số nguyên
+        const numericValue = parseInt(inputValue, 10);
+
+        // Kiểm tra nếu giá trị là một số hợp lệ
+        if (!isNaN(numericValue)) {
+            // Định dạng số với dấu phẩy ngăn cách phần nghìn
+            const formattedValue = new Intl.NumberFormat('en-US').format(numericValue);
+
+            // Cập nhật giá trị trong state với số thay vì chuỗi
+            setAmount(numericValue);
+            setMoneyInputAmount(formattedValue);
+        } else {
+            // Nếu giá trị không hợp lệ, có thể xử lý hoặc báo lỗi
+            setAmount(null); // hoặc có thể giữ nguyên giá trị hiện tại của state
+            setMoneyInputAmount('');
+        }
+    }
     const handleSubbmit = () => {
         setSubmitted(true)
     };
@@ -96,22 +128,22 @@ function NapTien() {
     }
     const handleSetNewSelectedAccount = (bankName, name, account) => {
         setShowBankDropDownMenu(false);
+        setShowInfor(true)
         setAdminBankNumber(account);
         setAdminName(name);
         setShowBankInforName(bankName);
+        setTextForSelectBank(bankName)
     }
 
-    console.log(adminBankNumber, adminName, bankBin, amount, userName);
-
     return (
-        <div className="ntWrapper">
-            <div className={cx("infoContainer")}>
+        <div className={cx("ntWrapper")}>
+            <div className={cx("infoContainer", "pc-only")}>
                 <div className={cx("ntTitle")}>Thông Tin Thiết Lập Mã QR</div>
                 <div className={cx("ntBox")}>
                     <div className={cx("select1")}>
                         <div className={cx("dropDownSelect")}
                             onClick={handleShowBankDropDownMenu}
-                        >Chọn Ngân Hàng Nhận
+                        >{textForSelectBank}
                             {!showBankDropDownMenu && <img src={arrowLeft}></img>}
                             {showBankDropDownMenu && <img src={arrowDown}></img>}
                         </div>
@@ -154,15 +186,84 @@ function NapTien() {
                         >
                         </input>
                     </div>
-                        <button className={cx(userName.length > 9 && amount && "ready")} onClick={handleSubbmit}>Tạo Mã</button>
+                    <button className={cx(userName.length > 9 && amount && "ready")} onClick={handleSubbmit}>Tạo Mã</button>
                 </div>
 
             </div>
-            <div className={cx("qrContainer")}>
+            <div className={cx("qrContainer", "pc-only")}>
                 <div>Mã QR Của Bạn</div>
                 {submitted && (
                     <img src={qrURL} alt="qr-code"></img>
                 )}
+                <button>Lưu Mã QR(Ảnh)</button>
+            </div>
+            {qrGenerated && (
+                <div className={cx("mbContainer2", "mobile-only")}>
+                    <div>Mã QR Của Bạn</div>
+                    <img src={qrURL} alt="qr-code"></img>
+                    <button onClick={handleSaveQR}>Lưu Mã QR(Ảnh)</button>
+                </div>
+            )}
+            <div className={cx("mbContainer1", "mobile-only")}>
+                <div className={cx("mbTitle")}>Thông Tin Thiết Lập Mã QR</div>
+                <div className={cx("mbContent")}>
+                    <div className={cx("mbInputBox1")}>
+                        <label for="userName">Nội Dung Chuyển Khoản: </label>
+                        <input
+                            maxLength="10"
+                            id="userName"
+                            required
+                            placeholder="Tên Đăng Nhập(Số Điện Thoại Đăng Kí)"
+                            type="tel"
+                            pattern="[0-9]*"
+                            // value={userName}
+                            onChange={changeUserNameHandler}
+                        ></input>
+                        {/* <div>*Không Thể Để Trống</div> */}
+                    </div>
+                    <div className={cx("mbInputBox2")}>
+                        <label for="amount">Số Tiền: </label>
+                        <input
+                            id="amount"
+                            required
+                            placeholder="Số Tiền (vnđ)"
+                            type="text"
+                            inputMode="numeric"
+                            value={moneyInputAmount}
+                            onChange={changeAmountHandler}
+                        >
+                        </input>
+                        {/* <div>*Không Thể Để Trống</div> */}
+                    </div>
+                    <div className={cx("mbSelect1")}>
+                        <div className={cx("dropDownSelect")}
+                            onClick={handleShowBankDropDownMenu}
+                        >{textForSelectBank}
+                            {!showBankDropDownMenu && <img src={arrowLeft}></img>}
+                            {showBankDropDownMenu && <img src={arrowDown}></img>}
+                        </div>
+
+                    </div>
+                    <div className={cx("dropDownMenu")}>
+                        {showBankDropDownMenu && (
+                            adminBankName.map((name, index) => (
+                                <div className={cx("dropDownElement")}
+                                    key={index}
+                                    onClick={() => handleSetNewSelectedAccount(name, adminBankAccountName[index], adminBankAccount[index])}
+                                >{name}</div>
+                            ))
+                        )}
+                    </div>
+                    {showInfor && (
+                        <div className={cx("showAdminInfoBox")}>
+                            <div className={cx("showAdminInfo1")}><p>{bankList.filter(bank => bank.shortName === showBankInforName)[0].name}</p></div>
+                            <div className={cx("showAdminInfo2")}>Số Tài Khoản Nhận:&nbsp;<p>{adminBankNumber}</p></div>
+                            <div className={cx("showAdminInfo3")}>Chủ Tài Khoản:&nbsp;<p>{adminName}</p></div>
+                        </div>
+                    )}
+
+                    <button className={cx(userName.length > 9 && amount && "ready")} onClick={handleSubbmit}>Tạo Mã {qrGenerated && " (Đã Tạo)"}</button>
+                </div>
             </div>
         </div>
     );
