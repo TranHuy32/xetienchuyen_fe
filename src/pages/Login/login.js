2be8090a4522c88ec753ec02da1685c45de68af3
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-12-08 20:03:56 +0700

    get otp for login

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -20,7 +20,8 @@ function Login() {
   });
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    concatenateValues()
     try {
       const response = await axios.post(`${beURL}/users-auth/loginOwner`, formData);
       const { accessToken, refreshToken, user } = response.data;
@@ -48,8 +49,53 @@ function Login() {
     }
   };
 
-  function clickEvent(event, nextInputId, prevInputId) {
+  console.log(formData);
+
+  const concatenateValues = () => {
+    const input1Value = document.getElementById("text1").value;
+    const input2Value = document.getElementById("text2").value;
+    const input3Value = document.getElementById("text3").value;
+    const input4Value = document.getElementById("text4").value;
+    const input5Value = document.getElementById("text5").value;
+    const input6Value = document.getElementById("text6").value;
+
+    const isNumericInRange = (value) => {
+      return /^\d$/.test(value) && Number(value) >= 0 && Number(value) <= 9;
+    };
+
+    if (
+      !isNumericInRange(input1Value) ||
+      !isNumericInRange(input2Value) ||
+      !isNumericInRange(input3Value) ||
+      !isNumericInRange(input4Value) ||
+      !isNumericInRange(input5Value) ||
+      !isNumericInRange(input6Value)
+    ) {
+      alert("Mã Xác Thực Không Hợp Lệ")
+    } else {
+      const concatenatedString = `${input1Value}${input2Value}${input3Value}${input4Value}${input5Value}${input6Value}`;
+      setFormData({
+        ...formData,
+        twoFaCode: concatenatedString,
+      })
+      return formData;
+    }
+  };
+
+  function inputEvent(event, nextInputId, prevInputId) {
     const currentInput = event.target;
+
+    if (event.key === 'Backspace' && currentInput.value.length === 0) {
+      const prevInput = document.getElementById(prevInputId);
+      if (prevInput) {
+        prevInput.focus();
+      }
+      return;
+    }
+
+    if (!/^\d$/.test(currentInput.value)) {
+      return; 
+    }
 
     if (currentInput.value >= 0 && currentInput.value <= 9) {
       const nextInput = document.getElementById(nextInputId);
@@ -58,18 +104,6 @@ function Login() {
       }
     } else {
       return;
-    }
-
-    if (event.key === 'Backspace' && currentInput.value.length === 0) {
-      const prevInput = document.getElementById(prevInputId);
-      if (prevInput) {
-        prevInput.focus();
-      }
-      return; // Stop further execution
-    }
-
-    if (!/^\d$/.test(currentInput.value)) {
-      return; // Return early if the entered value is not a digit
     }
   };
 
@@ -123,14 +157,14 @@ function Login() {
                     <div className={cx("FAContainer")}>
                       <div className={cx("FATitle")}>Nhập Mã Xác Thực 2FA: </div>
                       <div className={cx("inputContainer")}>
-                        <input type="number" pattern="[0-9]" id="text1" onKeyUp={(e) => clickEvent(e, "text2", "text1")} autoFocus />
-                        <input type="number" pattern="[0-9]" id="text2" onKeyUp={(e) => clickEvent(e, "text3", "text1")} />
-                        <input type="number" pattern="[0-9]" id="text3" onKeyUp={(e) => clickEvent(e, "text4", "text2")} />
-                        <input type="number" pattern="[0-9]" id="text4" onKeyUp={(e) => clickEvent(e, "text5", "text3")} />
-                        <input type="number" pattern="[0-9]" id="text5" onKeyUp={(e) => clickEvent(e, "text6", "text4")} />
-                        <input type="number" pattern="[0-9]" id="text6" onKeyUp={(e) => clickEvent(e, "submit", "text5")} />
+                        <input type="number" pattern="[0-9]" id="text1" onKeyUp={(e) => inputEvent(e, "text2", "text1")} autoFocus />
+                        <input type="number" pattern="[0-9]" id="text2" onKeyUp={(e) => inputEvent(e, "text3", "text1")} />
+                        <input type="number" pattern="[0-9]" id="text3" onKeyUp={(e) => inputEvent(e, "text4", "text2")} />
+                        <input type="number" pattern="[0-9]" id="text4" onKeyUp={(e) => inputEvent(e, "text5", "text3")} />
+                        <input type="number" pattern="[0-9]" id="text5" onKeyUp={(e) => inputEvent(e, "text6", "text4")} />
+                        <input type="number" pattern="[0-9]" id="text6" onKeyUp={(e) => inputEvent(e, "submit", "text5")} />
                       </div>
-                      <button type="submit" value="Log in">
+                      <button type="submit" value={"Log in"}>
                         Xác Nhận
                       </button>
                     </div>
