2a89c2aca46f4c3bd23db5ce9f1344d8c572f287
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2024-01-05 21:25:18 +0700

    add base userManual

--- src/pages/UserManual/index.js
+++ src/pages/UserManual/index.js
@@ -1,8 +1,11 @@
 import styles from "./usermanual.scss";
 import classNames from "classnames";
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
+import arrowLeft from "~/assets/image/left-arrow.png";
+import arrowDown from "~/assets/image/arrow-down.png";
 
 const cx = classNames.bind(styles);
 const beURL = process.env.REACT_APP_BE_URL;
@@ -10,17 +13,17 @@ const beURL = process.env.REACT_APP_BE_URL;
 function UsersManual() {
     const navigate = useNavigate();
     const [allowedToDisplay, setAllowToDisplay] = useState(false)
-    const [wdToken,setWdToken] =useState(null)
+    const [wdToken, setWdToken] = useState(null)
     const [userName, setUserName] = useState(null)
+    const [showManual, setShowManual] = useState("")
     useEffect(() => {
         axios
             .get(`${beURL}/users/depositStatus`)
             .then((response) => {
                 const data = response.data;
-                console.log(data);
-                if(data.success === 1){
+                if (data.success === 1) {
                     setAllowToDisplay(data.data.depositStatus)
-                }else{
+                } else {
                     setAllowToDisplay(false)
                 }
             })
@@ -28,7 +31,7 @@ function UsersManual() {
                 console.log(error);
             });
     }, []);
-    
+
     useEffect(() => {
         // Lấy URL hiện tại
         const currentUrl = window.location.href;
@@ -46,20 +49,114 @@ function UsersManual() {
         setWdToken(Token)
         setUserName(Name)
         //Thông báo thiếu thông tin
-        if(Token.length < 10 || Name.length < 10){
+        if (Token.length < 10 || Name.length < 10) {
             alert("Thông Tin Bị Thiếu! Hãy Mở Lại Trang Web Này Từ Ứng Dụng Của Bạn.")
             return
         }
     }, []);
 
+    const handleOpenManual = (id) => {
+        if (showManual === id) {
+            const activeElement = document.getElementById("active");
+            if (activeElement) {
+                activeElement.scrollIntoView({ behavior: "smooth",  marginTop: 120 });
+            }
+            return
+        } else {
+            setShowManual(id)
+            const activeElement = document.getElementById("active");
+            if (activeElement) {
+                activeElement.scrollIntoView({ behavior: "smooth",  marginTop: 120});
+            }
+            return
+        }
+    }
+
+    const handleCloseManual = (id) => {
+        if(showManual === id){
+            setShowManual("")
+        }else{
+            handleOpenManual(id)
+        }
+    }
+
     return (
-        <div className={cx("manualWrapper", allowedToDisplay ? "" : "notAllowedToDisplay")}>
-            <div className={cx("buttonContainer")}>
-                <button id="naptien" onClick={() => navigate(`/usermanual/naptien?userName=${userName}`)}>Nạp Tiền</button>
-                <button id="ruttien" onClick={() => navigate(`/usermanual/ruttien?wdToken=${wdToken}&userName=${userName}`)}>Rút Tiền</button>
+        <div className={cx("manualWrapper")}>
+            <h1>Hướng Dẫn Sử Dụng</h1>
+            <div
+            // className={cx(allowedToDisplay ? "" : "notAllowedToDisplay")}
+            >
+                <div className={cx("buttonContainer")}>
+                    <button id="naptien" onClick={() => navigate(`/usermanual/naptien?userName=${userName}`)}>Nạp Tiền</button>
+                    <button id="ruttien" onClick={() => navigate(`/usermanual/ruttien?wdToken=${wdToken}&userName=${userName}`)}>Rút Tiền</button>
+                </div>
             </div>
-            <div className={cx("userManualContainer")}></div>
+            <div className={cx("userManualContainer")}>
+                <div className={cx("manualContainer")} id={showManual === "NUMBER1" ? "active" : ""}>
+                    <div className={cx("mTitle")} onClick={() => handleOpenManual("NUMBER1")}>
+                        Hướng Dẫn Đổi Mật Khẩu
+                        <img src={showManual === "NUMBER1" ? arrowDown : arrowLeft}
+                            onClick={() => handleCloseManual("NUMBER1")}
+                        ></img>
+                    </div>
+                    {showManual === "NUMBER1" && (
+                        <div className={cx("mContent")}>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 1:</strong> Dưới đây là một số trang web phổ biến cung cấp hướng dẫn sử dụng phần mềm hoặc user manual với giao diện dành cho thiết bị di động:</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 2:</strong> làm abc xyz</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 3:</strong> làm abc xyz</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+                </div>
+                <div className={cx("manualContainer")} id={showManual === "NUMBER2" ? "active" : ""}>
+                    <div className={cx("mTitle")} onClick={() => handleOpenManual("NUMBER2")}>
+                        Hướng Dẫn Tạo Đơn Chở Khách
+                        <img src={showManual === "NUMBER2" ? arrowDown : arrowLeft}
+                         onClick={() => handleCloseManual("NUMBER2")}
+                        ></img>
+                    </div>
+                    {showManual === "NUMBER2" && (
+                        <div className={cx("mContent")}>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 1:</strong> Dưới đây là một số trang web phổ biến cung cấp hướng dẫn sử dụng phần mềm hoặc user manual với giao diện dành cho thiết bị di động:</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 2:</strong> làm abc xyz</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                            <div className={cx("stepBox")}>
+                                <div className={cx("stepText")}><strong>bước 3:</strong> làm abc xyz</div>
+                                <div className={cx("stepImageBox")}>
+                                    <div className={cx("img")}></div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+                </div>
+            </div>
         </div>
+
     );
 }
 
